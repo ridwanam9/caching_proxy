b6ccc29cbc9eb626e94a08d3dfdeceaf2a8fcae3
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-09-05 17:27:02 +0700

    refactore: fixing basic cli

--- app/cli.py
+++ app/cli.py
@@ -9,21 +9,34 @@ def main():
     parser.add_argument(
         "--port",
         type=int,
-        required=True,
+        # required=True,
         help="Port where the proxy server will run"
     )
 
     parser.add_argument(
         "--origin",
-        required=True,
+        # required=True,
         help="Origin server URL"
     )
+    parser.add_argument(
+        "--clear-cache",
+        action="store_true",
+        help="Clear the cache"
+    )
+
 
     args = parser.parse_args()
 
+    if args.clear_cache:
+        print("Cache cleared")
+        return
+    
+    if args.port is None or args.origin is None:
+        parser.error("--port and --origin are required")
+    
+    
     print(f"Port: {args.port}")
     print(f"Origin: {args.origin}")
-
 
 if __name__ == "__main__":
     main()
