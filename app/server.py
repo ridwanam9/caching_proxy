01e0f9d5f5c5e9a5c6550f4916e1cf6d285bf510
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-09-06 16:04:11 +0700

    refactor: add HIT and MISS response

--- app/server.py
+++ app/server.py
@@ -6,15 +6,34 @@ app = FastAPI()
 
 origin = None
 
+cache = {}
+
 
 @app.api_route("/{path:path}", methods=["GET"])
 async def proxy(path: str):
+    cache_key = path
+
+    if cache_key in cache:
+        print("CACHE HIT")
+
+        cached_response = cache[cache_key]
+
+        return JSONResponse(
+            content=cached_response,
+            headers={"X-Cache": "HIT"}
+        )
+
+    print("CACHE MISS")
+
     target_url = f"{origin}/{path}"
 
     async with httpx.AsyncClient() as client:
         response = await client.get(target_url)
 
+    cache[cache_key] = response.json()
+
     return JSONResponse(
         content=response.json(),
-        status_code=response.status_code
+        status_code=response.status_code,
+        headers={"X-Cache": "MISS"}
     )
