4ccabaa5731df9c44eee6c381f342a8f6d324d1f
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-09-06 15:44:14 +0700

    add: products path

--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+# import httpx
 
 app = FastAPI()
 
@@ -6,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Caching proxy is running"}
+
+@app.api_route("/{path:path}", methods=["GET"])
+async def proxy(path: str):
+    print(f"Requested path: {path}")
+
+    
